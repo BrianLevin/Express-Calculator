4fdc97c4700cf083495dc13345c059e2f18ae9d0
BrianLevin <bml201095@gmail.com>
2021-04-15 16:21:45 -0400

    created variables which will hold and pass the parsed values

--- calculator.js
+++ calculator.js
@@ -13,9 +13,13 @@ app.get("/", function(req,res){
 
 app.post ("/", function (req,res){
 
-    console.log(req.body.num1);
+    var num1= req.body.num1;
 
-res.send("Thanks for posting ");
+    var num2 = req.body.num2;
+
+    var result=  num1 + num2;
+
+res.send("Th e result of the calculation is: " + result);
 });
 
 app.listen(3000, function(){
